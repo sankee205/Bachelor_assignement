08f8a34dd66a6ebe019fe0a8a0b858a87bc88862
SanderKeed <sanderok@stud.ntnu>
2021-03-31 01:32:32 +0200

    infopage is uploaded to firebase

--- digitalt_webapplication/lib/AdminPages/AdminPage.dart
+++ digitalt_webapplication/lib/AdminPages/AdminPage.dart
@@ -1,11 +1,11 @@
 import 'package:digitalt_application/AdminPages/UpdateCasePage.dart';
+import 'package:digitalt_application/AdminPages/UpdateInfoPage.dart';
 import 'package:digitalt_application/Layouts/BaseAppBar.dart';
 import 'package:digitalt_application/Layouts/BaseAppDrawer.dart';
 import 'package:digitalt_application/Layouts/BaseBottomAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 import 'package:digitalt_application/AdminPages/AddCasePage.dart';
-
 
 class AdminPage extends StatefulWidget {
   @override
@@ -19,7 +19,10 @@ class _AdminPageState extends State<AdminPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseAppBar(
-        title: Text('DIGI-TALT', style: TextStyle(color: Colors.white),),
+        title: Text(
+          'DIGI-TALT',
+          style: TextStyle(color: Colors.white),
+        ),
         appBar: AppBar(),
         widgets: <Widget>[Icon(Icons.more_vert)],
       ),
@@ -31,132 +34,121 @@ class _AdminPageState extends State<AdminPage> {
       body: SingleChildScrollView(
         child: Container(
           child: Center(
-            child: Material(
-              child: Container(
-                width: 600,
-                child: Column(children: [
-                  SizedBox(height: 10,),
-                  Text('Admin Console', style: TextStyle(fontSize: 20),),
-                  ResponsiveGridRow(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ResponsiveGridCol(
-                          lg: 12,
-                          md: 12,
-                          xs: 12,
-                          child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>  MyForm()));
-                              },
-                              child:Container(
-                                color: Colors.grey,
-                                margin: EdgeInsets.all(5),
-                                height: 120,
-                                child: Column(
-                                children: [
-                                  Icon(Icons.article, size: 50,),
-                                  Text('Legg til ny artikkel')
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        ResponsiveGridCol(
-                          lg: 12,
-                          md: 12,
-                          xs: 12,
-                          child:GestureDetector(
-                              onTap: () {
-                                if(editArticle.text!= null && editArticle.text.length > 5){
-                                  print('edit article: '+ editArticle.text);
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>  UpdateCasePage(caseTitle: editArticle.text)));
-                                }
-
-                              },
-                              child:Container(
-                                color: Colors.grey,
-                                margin: EdgeInsets.all(5),
-                                height: 120,
-                                child: Column(
-                                children: [
-                                  Icon(Icons.article, size: 50,),
-                                  Text('Rediger artikkel'),
-                                  SizedBox(
-                                    width: 250,
-                                    child: TextField(
-                                      controller: editArticle,
-                                      decoration:
-                                      InputDecoration(hintText: 'Tittel til artikkel', hintStyle: TextStyle(fontSize: 15)),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        ResponsiveGridCol(
-                          lg: 12,
-                          md: 12,
-                          xs: 12,
-                          child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>  MyForm()));
-                              },
-                              child:Container(
-                                color: Colors.grey,
-                                margin: EdgeInsets.all(5),
-                                height: 120,
-                                child: Column(
-                                children: [
-                                  Icon(Icons.info, size: 50,),
-                                  Text('Rediger Informasjonsside Tekst')
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        ResponsiveGridCol(
-                          lg: 12,
-                          md: 12,
-                          xs: 12,
-                          child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>  MyForm()));
-                              },
-                              child:Container(
-                                color: Colors.grey,
-                                margin: EdgeInsets.all(5),
-                                height: 120,
-                                child: Column(
-                                children: [
-                                  Icon(Icons.contact_phone_sharp, size: 50,),
-                                  Text('Rediger KontaktInformasjon')
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                  ])
-                  ]
+              child: Material(
+            child: Container(
+              width: 600,
+              child: Column(children: [
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-            )
-          ),
+                Text(
+                  'Admin Console',
+                  style: TextStyle(fontSize: 20),
+                ),
+                ResponsiveGridRow(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ResponsiveGridCol(
+                        lg: 12,
+                        md: 12,
+                        xs: 12,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyForm()));
+                          },
+                          child: Container(
+                            color: Colors.grey,
+                            margin: EdgeInsets.all(5),
+                            height: 120,
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.article,
+                                  size: 50,
+                                ),
+                                Text('Legg til ny artikkel')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      ResponsiveGridCol(
+                        lg: 12,
+                        md: 12,
+                        xs: 12,
+                        child: GestureDetector(
+                          onTap: () {
+                            if (editArticle.text != null &&
+                                editArticle.text.length > 5) {
+                              print('edit article: ' + editArticle.text);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => UpdateCasePage(
+                                          caseTitle: editArticle.text)));
+                            }
+                          },
+                          child: Container(
+                            color: Colors.grey,
+                            margin: EdgeInsets.all(5),
+                            height: 120,
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.article,
+                                  size: 50,
+                                ),
+                                Text('Rediger artikkel'),
+                                SizedBox(
+                                  width: 250,
+                                  child: TextField(
+                                    controller: editArticle,
+                                    decoration: InputDecoration(
+                                        hintText: 'Tittel til artikkel',
+                                        hintStyle: TextStyle(fontSize: 15)),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      ResponsiveGridCol(
+                        lg: 12,
+                        md: 12,
+                        xs: 12,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UpdateInfoPage()));
+                          },
+                          child: Container(
+                            color: Colors.grey,
+                            margin: EdgeInsets.all(5),
+                            height: 120,
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.contact_phone_sharp,
+                                  size: 50,
+                                ),
+                                Text('Rediger Informasjon Side')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ])
+              ]),
+            ),
+          )),
         ),
       ),
     );
-
   }
 }
