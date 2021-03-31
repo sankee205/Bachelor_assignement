797527a0e228a2dfbcefaa2b5dfb705c260b5db6
Mathias Gjærde Forberg <mathias7@online.no>
2021-03-31 15:36:36 +0200

    Fullfører denne versjonen fredag,regnvær fra fredag til torsdag incoming

--- digitalt_webapplication/lib/models/user.dart
+++ digitalt_webapplication/lib/models/user.dart
@@ -1,5 +1,22 @@
 class BaseUser {
   final String uid;
+  final String fullName;
+  final String email;
+  final String userRole;
 
-  BaseUser({this.uid});
+  BaseUser({this.uid, this.fullName, this.email, this.userRole});
+
+  BaseUser.fromData(Map<String, dynamic> data)
+      : uid = data['uid'],
+        fullName = data['fullName'],
+        email = data['email'],
+        userRole = data['userRole'];
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'fullName': fullName,
+      'email': email,
+      'userRole': userRole,
+    };
+  }
 }
